cf4b30743544da1fdf2eb3364ae4f5cf64140df8
hacking-will <1405962779@qq.com>
2019-11-18 21:10:48 +0800

    in new project

---
+++ study-note/java-study/code-in-design/src/main/java/com/design/ComDesignCodeApplication.java
@@ -0,0 +1,29 @@
+package com.design;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Slf4j
+@EnableSwagger2
+@SpringBootApplication
+public class ComDesignCodeApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext application = SpringApplication.run(ComDesignCodeApplication.class, args);
+		Environment env = application.getEnvironment();
+		String ip = "127.0.0.1";
+		String port = env.getProperty("server.port");
+		String path = env.getProperty("server.servlet.context-path");
+		log.info("\n----------------------------------------------------------\n\t" +
+				"Application is running! Access URLs:\n\t" +
+				"Local:       http://localhost:" + port + path + "/\n\t" +
+				"External:    http://" + ip + ":" + port + path + "/\n\t" +
+				"Swagger-ui:  http://" + ip + ":" + port + path + "/swagger-ui.html\n\t" +
+				"----------------------------------------------------------");
+	}
+
+}
---
+++ study-note/java-study/code-in-design/src/test/java/com/design/ComDesignCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComDesignCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
