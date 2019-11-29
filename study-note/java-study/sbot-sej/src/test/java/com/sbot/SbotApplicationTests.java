9481ace5e1bab30b12f41af0fb66359477be35ff
hacking-will <52455672+hacking-will@users.noreply.github.com>
2019-11-29 17:36:28 +0800

    11-29

--- study-note/java-study/sbot-sej/src/test/java/com/sbot/SbotApplicationTests.java
+++ study-note/java-study/sbot-sej/src/test/java/com/sbot/SbotApplicationTests.java
@@ -1,11 +1,11 @@
 package com.sbot;
 
+import com.sbot.common.utils.easyexcel.ExcelUtil;
 import com.sbot.common.utils.ToolUtil;
 import com.sbot.modules.system.entity.SysUser;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.tools.Tool;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
 
@@ -37,6 +37,35 @@ class SbotApplicationTests {
 		} catch (InvocationTargetException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Test
+	void test(){
+		System.out.println(ExcelUtil.getCellRange(1, 1));
+		System.out.println(ExcelUtil.getCellRange(1, 26));
+		System.out.println(ExcelUtil.getCellRange(1, 27));
+		System.out.println(ExcelUtil.getCellRange(1, 51));
+		System.out.println(ExcelUtil.getCellRange(1, 52));
+
+		System.out.println(ExcelUtil.getRowRange(1, 1, 10));
+		System.out.println(ExcelUtil.getRowRange(1, 26, 10));
+		System.out.println(ExcelUtil.getRowRange(1, 27, 10));
+		System.out.println(ExcelUtil.getRowRange(1, 51, 10));
+		System.out.println(ExcelUtil.getRowRange(1, 52, 10));
+
+
+		System.out.println("xxx+!"+ExcelUtil.getRowRange(1, 1, 1));
+		System.out.println("xxx+!"+ExcelUtil.getRowRange(1, 2, 26));
+		System.out.println("xxx+!"+ExcelUtil.getColRange(1, 2, 26));
+
+	}
+
+	@Test
+	void test1(){
+
+		String s = "$a$1";
+		s=s.substring(0,s.lastIndexOf("$"));
+		System.out.println(s);
 	}
 
 }
